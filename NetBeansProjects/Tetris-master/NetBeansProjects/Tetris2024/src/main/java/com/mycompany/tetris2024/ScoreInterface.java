027efb0872978cd6dc6d644c851b99786ad3466f
Tanotom <sebgonosp@alu.edu.gva.es>
2024-02-12 20:26:27 +0100

    Initial commit

---
+++ NetBeansProjects/Tetris-master/NetBeansProjects/Tetris2024/src/main/java/com/mycompany/tetris2024/ScoreInterface.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.tetris2024;
+
+/**
+ *
+ * @author alu10654799
+ */
+public interface ScoreInterface {
+    public void incrementScore();
+    public void reset();
+}
---
+++ NetBeansProjects/Tetris-master/NetBeansProjects/Tetris2024/src/main/java/com/mycompany/tetris2024/Tetrominoes.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package com.mycompany.tetris2024;
+
+/**
+ *
+ * @author alu10654799
+ */
+public enum Tetrominoes {
+    NoShape, ZShape, SShape,
+    LineShape, TShape, SquareShape, LShape,
+    MirroredLShape 
+}
